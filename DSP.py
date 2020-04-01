42f02a9c20119976135ee44ebf209e10aecc013e
Sofia Fatigoni <sofi@heyheysp.local>
2020-03-31 19:49:39 -0700

    added class DSP

--- DSP.py
+++ DSP.py
@@ -1,12 +1,10 @@
 import numpy as np
 
 
-class DSP(ogbject):
+class DSP(object):
     '''
     DO DPS operation and eventually simulate signal
     '''
-    def trivial_op(a,b):
-        return a * b
 
     def __init__(self, NX, NY, inner_length):
         '''
@@ -18,22 +16,25 @@ class DSP(ogbject):
         '''
         self.Nx = NX
         self.Ny = NY
-        self.signal = np.zeros(self.Nx, self.Ny, 2, inner_length)
+        self.signal = np.zeros((self.Nx, self.Ny, 2, inner_length))
+        #print(self.signal)
 
-    def get_triangle(self, f_low, f_high, norm_low, norm_high):
+    def get_triangle(self, f_low, f_high, lenght):
         '''
-
         Return NX x NY x 2 tensor in 0 - 1 interval
         '''
-
-        fft = np.fft.rfft(self.signal, axis = 3)
-        fft = np.sum()
-        return fft
+        self.gen_signal(lenght)
+        signal_FT = np.fft.rfft(self.signal, axis = 3)
+        power_spect=np.sum((np.abs(signal_FT)**2)[f_low:f_high], axis=3)
+        #print(np.shape(power_spect))
+        return power_spect
 
     def gen_signal(self, length):
         '''
         Generate signal for length time interval.
         '''
-        signal = np.array(self.Nx, self.Ny, 2, length)
-        self.signal = np.roll(length, axis = 3)
+        signal=np.random.normal(10, 1, size=(self.Nx, self.Ny, 2, length))
+        self.signal = np.roll(self.signal, shift=length, axis = 3)
         self.signal[:,:,:,0:length] = signal
+        #print(self.signal[:,:,:,0:length])
+        #print(self.signal[:,:,:,length:2*length])
