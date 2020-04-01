459d2c7c1b93069dcad10d4b9d042a641b52fa31
Lorenzo <minutolo@caltech.edu>
2020-03-31 19:50:41 -0700

    Added triangle view

--- static/js/triangle_view.js
+++ static/js/triangle_view.js
@@ -1,11 +1,21 @@
 // Box dimensions
-var total_height = 1000;
-var total_width = 1000;
+var total_height = 1000.;
+var total_width = 1000.;
 
-var total_step_x = 4;
-var total_step_y = 4;
+var total_step_x = 20.;
+var total_step_y = 20.;
+
+var border_size = 10
+// index to triangle 2d index
+var triangle_pos = Array(0)
+var triangle_size_x = (total_width - total_step_x*border_size)/total_step_x;
+var triangle_size_y = (total_height - total_step_y*border_size)/total_step_y
+
+var triangle_step_x = (total_width)/total_step_x;
+var triangle_step_y = (total_height)/total_step_y
 
 
+var color_scale = chroma.scale(['black', 'red']);
 
 var svgContainer = d3.select("div#triangle_viewer")
  .append("div")
@@ -18,9 +28,98 @@ var svgContainer = d3.select("div#triangle_viewer")
  // Class to make it responsive.
  .classed("svg-content-responsive", true)
 
+// where x and y are indexes
+function make_triangle_position_polA(x, y){
+  var start_x = triangle_size_x*x// + x*border_size;
+  var end_x = start_x + triangle_size_x;
 
-function svgContainer.append("path")
-      .attr("d", " M 10 25 L 10 75 L 60 75 L 10 25")
-      .attr("stroke", "red")
-      .attr("stroke-width", 2)
-      .attr("fill", "white")
+  var start_y = triangle_size_y*y// + y*border_size;
+  var end_y = start_y + triangle_size_y;
+  var set_string = " M "+ start_x +" "+ start_y +" L "+ start_x +" "+ end_x +" L "+ end_y +" "+ end_x +" L "+ start_x +" "+ start_y;
+  // console.log(set_string)
+  return set_string
+}
+function make_triangle_position_polB(x, y){
+  var start_x = triangle_size_x*x// + x*border_size;
+  var end_x = start_x + triangle_size_x;
+
+  var start_y = triangle_size_y*y// + y*border_size;
+  var end_y = start_y + triangle_size_y;
+  var set_string = " M "+ start_x +" "+ start_y +" L "+ start_x +" "+ end_x +" L "+ end_y +" "+ end_x +" L "+ start_x +" "+ start_y;
+  var set_string = " M "+ start_x +" "+ start_y +" L "+ end_x +" "+ start_x +" L "+ end_y +" "+ end_x +" L "+ start_x +" "+ start_y;
+  // console.log(set_string)
+  return set_string
+}
+
+function make_tanslate(x, y){
+  var x_pos = (triangle_step_x*x)// + (x*border_size)
+  var y_pos = (triangle_step_y*y)// + (y*border_size)
+  var str =  "translate(" + x_pos + " "+ y_pos +")";
+  return str
+}
+function make_triangleA(x,y){
+  var polA = svgContainer.append("path")
+        // .attr("d", " M 10 25 L 10 75 L 60 75 L 10 25")
+        .attr("d", make_triangle_position_polA(0, 0))
+        .attr("stroke", "white")
+        .attr("stroke-width", 2)
+        .attr("fill", "black")
+        .attr("transform", make_tanslate(x, y))
+  polA.on("click", function(){
+
+    svgContainer.selectAll("path")
+      .attr("stroke", "white")
+      .lower()
+    polA.raise();
+    // selected_tile = Array(0);
+    // selected_tile.push(rect.attr("id"))
+    return polA.attr("stroke", "grey");
+  })
+  return polA
+}
+function xy2n(x,y){
+  return x*total_step_y + y;
+}
+function make_triangleB(x,y){
+  var polB = svgContainer.append("path")
+        // .attr("d", " M 10 25 L 10 75 L 60 75 L 10 25")
+        .attr("d", make_triangle_position_polB(0, 0))
+        .attr("stroke", "white")
+        .attr("stroke-width", 2)
+        .attr("fill", "green")
+        .attr("transform", make_tanslate(x, y))
+  polB.on("click", function(){
+    svgContainer.selectAll("path")
+      .attr("stroke", "white")
+      .lower()
+    polB.raise();
+    // selected_tile = Array(0);
+    // selected_tile.push(rect.attr("id"))
+    return polB.attr("stroke", "grey");
+  })
+  return polB
+}
+
+for(var y = 0; y<total_step_y; y++){
+  for(var x = 0; x<total_step_x; x++){
+    triangle_pos.push({'x':x,'y':y, 'polA':make_triangleA(x,y), 'polB':make_triangleB(x,y)})
+  }
+}
+xxx = Array(0)
+for(var y = 0; y<total_step_y; y++){
+  for(var x = 0; x<total_step_x; x++){
+    xxx.push(parseFloat(100+x*y)/(total_step_x*total_step_y))
+  }
+}
+function update_colors(arr){
+  color_arr = (arr.map(color_scale)).map(x => x.hex());
+  for(var y = 0; y<total_step_y; y++){
+    for(var x = 0; x<total_step_x; x++){
+      var index = xy2n(x,y)
+      // console.log(index)
+      triangle_pos[index]['polA'].attr("fill", color_arr[index])
+      triangle_pos[index]['polB'].attr("fill", color_arr[index])
+    }
+  }
+}
+update_colors(xxx)
