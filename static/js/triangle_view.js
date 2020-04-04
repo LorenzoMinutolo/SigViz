aff4e931314b11984ea9c2607e03b88e4923e43a
Lorenzo <minutolo@caltech.edu>
2020-04-03 19:07:33 -0700

    Updated triangleview with tooltip and better selector

--- static/js/triangle_view.js
+++ static/js/triangle_view.js
@@ -14,9 +14,21 @@ var triangle_size_y = (total_height - total_step_y*border_size)/total_step_y
 var triangle_step_x = (total_width)/total_step_x;
 var triangle_step_y = (total_height)/total_step_y
 
+var selected_triangle = Array(0);
 
 // var color_scale = chroma.scale(['black','red','blue','yellow']);
 var color_scale = chroma.scale(['yellow', '008ae5']);
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
 
 function set_viridis_colorscale(){
   color_scale = chroma.scale(['yellow', '008ae5']);
@@ -40,6 +52,14 @@ var svgContainer = d3.select("div#triangle_viewer")
  .attr("viewBox", "0 0 "+ total_height +" " + total_width)
  // Class to make it responsive.
  .classed("svg-content-responsive", true)
+
+function triangle_xy2string(x, y, pol){
+  return x+'_'+y+'_'+pol
+}
+function triangle_string2xy(str){
+  var xx =  str.split("_")
+  return {'x':xx[0],'y':xx[1],'pol':xx[2]}
+}
 
 // where x and y are indexes
 function make_triangle_position_polA(x, y){
@@ -70,7 +90,14 @@ function make_tanslate(x, y){
   var str =  "translate(" + x_pos + " "+ y_pos +")";
   return str
 }
+function xy2n(x,y){
+  return x + (total_step_y)*y;
+}
+function format_tooltip(x,y,pol){
+  return "<b>row::</b> " + y + "<br><b>col:</b> " + x + "<br> <b>pol:</b> : "+pol
+}
 function make_triangleA(x,y){
+  var t_name = triangle_xy2string(x,y,"A");
   var polA = svgContainer.append("path")
         // .attr("d", " M 10 25 L 10 75 L 60 75 L 10 25")
         .attr("d", make_triangle_position_polA(0, 0))
@@ -78,22 +105,48 @@ function make_triangleA(x,y){
         .attr("stroke-width", 2)
         .attr("fill", "black")
         .attr("transform", make_tanslate(x, y))
+        polA.on("mouseover", function(){
+          tooltip.html(format_tooltip(x,y,"A"));
+          polA.raise();
+          polA.attr("stroke", "red");
+          return tooltip.style("visibility", "visible");
+        })
+        polA.on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX-200)+"px");})
+        polA.on("mouseout", function(){
+          if(selected_triangle.includes(t_name)){
+            polA.raise();
+            polA.attr("stroke", "black");
+          }else{
+            polA.lower();
+            polA.attr("stroke", "white");
+          }
+          return tooltip.style("visibility", "hidden");
+        })
   polA.on("click", function(){
+    // console.log(selected_triangle);
+    if(selected_triangle.includes(t_name)){
+      polA.lower();
+      selected_triangle.remove(t_name);
+      return polA.attr("stroke", "white");
 
+    }else{
+      polA.raise();
+      selected_triangle.push(t_name);
+      return polA.attr("stroke", "black");
+    }
     // svgContainer.selectAll("path")
     //   .attr("stroke", "white")
     //   .lower()
-    polA.raise();
+
+
     // selected_tile = Array(0);
     // selected_tile.push(rect.attr("id"))
-    return polA.attr("stroke", "grey");
+
   })
   return polA
 }
-function xy2n(x,y){
-  return x + (total_step_y)*y;
-}
 function make_triangleB(x,y){
+  var t_name = triangle_xy2string(x,y,"B");
   var polB = svgContainer.append("path")
         // .attr("d", " M 10 25 L 10 75 L 60 75 L 10 25")
         .attr("d", make_triangle_position_polB(0, 0))
@@ -101,14 +154,36 @@ function make_triangleB(x,y){
         .attr("stroke-width", 2)
         .attr("fill", "green")
         .attr("transform", make_tanslate(x, y))
-  polB.on("click", function(){
-    // svgContainer.selectAll("path")
-    //   .attr("stroke", "white")
-    //   .lower()
+
+  polB.on("mouseover", function(){
     polB.raise();
-    // selected_tile = Array(0);
-    // selected_tile.push(rect.attr("id"))
-    return polB.attr("stroke", "grey");
+    polB.attr("stroke", "red");
+    tooltip.html(format_tooltip(x,y,"B"));
+    return tooltip.style("visibility", "visible");
+  })
+  polB.on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX-200)+"px");})
+  polB.on("mouseout", function(){
+    if(selected_triangle.includes(t_name)){
+      polB.raise();
+      polB.attr("stroke", "black");
+    }else{
+      polB.lower();
+      polB.attr("stroke", "white");
+    }
+    return tooltip.style("visibility", "hidden");
+  })
+
+  polB.on("click", function(){
+    // console.log(selected_triangle);
+    if(selected_triangle.includes(t_name)){
+      polB.lower();
+      selected_triangle.remove(t_name);
+      return polB.attr("stroke", "white");
+    }else{
+      polB.raise();
+      selected_triangle.push(t_name);
+      return polB.attr("stroke", "black");
+    }
   })
   return polB
 }
@@ -151,6 +226,14 @@ socket.on( 'triangle_data', function( msg ) {
   update_colors(msg_json)
   // setTimeout(() => {  socket.emit('get_triangle', {}) }, 100);
   socket.emit('get_triangle', {})
-
-
 });
+
+var tooltip = d3.select("div#triangle_viewer")
+	.append("div")
+	.style("position", "absolute")
+	.style("z-index", "10")
+  .style("border-style", "solid")
+  .style("background-color", "white")
+  .style("padding", "10px")
+  .style("opacity", "0.8")
+	.style("visibility", "hidden")
