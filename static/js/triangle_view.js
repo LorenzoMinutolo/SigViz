9171545999ecc4c8c2bd7e62c01f28bfee807c73
Lorenzo Minutolo <minutolo@caltech.edu>
2020-04-14 00:47:43 -0700

    Added signal selection logic. Conditionally activate the plotter button.
    NOTE: this is quite bugged: now the DSP needs a lock and an exchange 
    socket for plot generation.

--- static/js/triangle_view.js
+++ static/js/triangle_view.js
@@ -15,9 +15,21 @@ var triangle_step_x = (total_width)/total_step_x;
 var triangle_step_y = (total_height)/total_step_y
 
 var selected_triangle = Array(0);
-var selected_signal = Array(0)
+var selected_signal = Array(0);
 
 var triangle_sig_mode = "data" //mode=0 for Bias; mode=else for signal
+
+var plot_mode = "ts" // To get the radio button mode plot select modal
+
+
+// docReady(
+  $('#plotter-btn').prop("onclick", null).off("click");
+  $('#plotter-btn').click(function() {
+       $("#modal_plot_select")
+       $("#modal_plot_launch").modal('show')
+  });
+// );
+
 // in the form of {
 //   "entity": timestream fft average std and so on,
 //   "target": [{'x':n,'y'n}]
@@ -32,6 +44,38 @@ function switch_triangle_mode_data(){
 function switch_triangle_mode_bias(){
   console.log("Switching to triangle mode: BIAS")
   triangle_sig_mode = "bias"
+}
+
+function update_sig_table(){
+  tmp_string = ""
+  for(var i = 0; i < selected_signal.length; i++){
+    tmp_string+= "<tr><td>"+i+"</td><td>"+selected_signal[i]["kind"]+"</td><td>"+selected_signal[i]["detectors"]+"</td></tr>"
+  }
+  $('#signal_selector_confirm_body').html(tmp_string);
+}
+
+function add_signal(){
+  var kind = $('input[name=mode_sel]:checked').val()
+  var signals = ""
+  for(var i = 0; i < selected_triangle.length; i++){
+    signals+= selected_triangle[i] + " ";
+  }
+  selected_signal.push(
+    {
+      'kind':kind,
+      'detectors':signals
+    }
+  )
+  console.log(selected_signal);
+  update_sig_table()
+  reset_selection()
+  $("#plotter-btn").html("Plotter ("+selected_signal.length+")")
+}
+
+function reset_all_signals(){
+  selected_signal = Array(0);
+  $("#plotter-btn").html("Plotter (0)");
+  $('#signal_selector_confirm_body').html("");
 }
 
 // var color_scale = chroma.scale(['black','red','blue','yellow']);
@@ -217,7 +261,9 @@ function make_triangleA(x,y){
   polA.on('contextmenu', d3.contextMenu(menu,{
     onOpen: function (data, index) {
           polA.raise();
-          selected_triangle.push(t_name);
+          if(!selected_triangle.includes(t_name)){
+            selected_triangle.push(t_name);
+          }
           polA.attr("stroke", "black");
 				}
   }));
@@ -284,7 +330,9 @@ function make_triangleB(x,y){
   polB.on('contextmenu', d3.contextMenu(menu,{
     onOpen: function (data, index) {
           polB.raise();
-          selected_triangle.push(t_name);
+          if(!selected_triangle.includes(t_name)){
+            selected_triangle.push(t_name);
+          }
           polB.attr("stroke", "black");
 				}
   }));
@@ -348,28 +396,35 @@ var tooltip = d3.select("div#triangle_viewer")
 
 function set_plotting_mode_ts(){
   $("#mode_descriptor").html("<b>Plotting mode description: </b> Each selected detector timestream will be dispayed as a trace in the plot.")
+  plot_mode = "ts"
 }
 
 function set_plotting_mode_ats(){
   $("#mode_descriptor").html("<b>Plotting mode description: </b> All selected detercor's timestream will be averaged in a single trace in the plot")
+  plot_mode = "ats"
 }
 
 function set_plotting_mode_ps(){
   $("#mode_descriptor").html("<b>Plotting mode description: </b> Each detector timestream power spectra will be dispayed as a trace in the plot.")
+  plot_mode = "ps"
 }
 
 function set_plotting_mode_aps(){
   $("#mode_descriptor").html("<b>Plotting mode description: </b> All selected detercor's timestream power spectra will be averaged in a single trace in the plot.")
+  plot_mode = "aps"
 }
 
 function set_plotting_mode_pd(){
   $("#mode_descriptor").html("<b>Plotting mode description: </b> Each trace in the plot will be the difference of two polarization timestrams per each selected detector pair.")
+  plot_mode = "pd"
 }
 
 function set_plotting_mode_apd(){
   $("#mode_descriptor").html("<b>Plotting mode description: </b> All selected detercor's pair difference timestream will be averaged in a single trace in the plot")
+  plot_mode = "apd"
 }
 
 function set_plotting_mode_pds(){
   $("#mode_descriptor").html("<b>Plotting mode description: </b> Each trace in the plot will be the power spectrum of the difference of two polarization timestrams per each selected detector pair.")
+  plot_mode = "pds"
 }
